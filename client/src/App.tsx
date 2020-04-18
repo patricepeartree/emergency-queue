5e08865761cd97d71524e1d7a0625bb2b36b40dc
Patrícia Pereira <patricia_alexandrapereira@hotmail.com>
2020-04-18 18:55:40 +0100

    #9: Sidebar with stats.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Sidebar, Segment, Header, Icon } from "semantic-ui-react";
+
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 
 import StatsSection from './containers/stats-section';
 
 function App() {
-  return (
-    <StatsSection />
-  );
+    return (
+        <>
+            <Header as="h2">
+                <Icon name='user doctor' />
+                <Header.Content>Emergency Queue</Header.Content>
+            </Header>
+
+            <Sidebar.Pushable as={Segment}>
+                <Sidebar
+                    animation="push"
+                    visible
+                    width="wide"
+                >
+                    <StatsSection />
+                </Sidebar>
+
+                <Sidebar.Pusher>
+                    <div style={{ height: "100vh" }}>
+                        TODO
+                    </div>
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
+        </>
+    );
 }
 
 export default App;

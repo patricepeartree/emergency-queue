bedefe8816d161e0da4a71ef88cc61046048978a
Leonor Bandeira <leonorflag@hotmail.com>
2020-04-22 23:05:21 +0100

    changed language of system to en-GB close #20 error handling done, in case of wrong message format and wrong request code

--- server/src/controllers/text-controller.ts
+++ server/src/controllers/text-controller.ts
@@ -1,5 +1,6 @@
 import {Twilio, twiml} from "twilio";
 import {TextService} from "../services";
+import {RequestErrors} from "../utils/request-util";
 
 const accountSid = 'AC47b85ae425ff7eaf33b7e7cec6677557';
 const authToken = '989248adebd4c3d425e095200c30cc40';
@@ -8,19 +9,27 @@ const client = new Twilio(accountSid, authToken);
 export function sendSmsId(smsId: number, to: string) {
    return  client.messages.create({
         from: '+48732483267',
-        body: 'A sua chamada para a Saúde 24 foi registada. ' +
-            'Para saber o tempo esperado para lhe ligarem, envie SMS ' +
-            `para 303 800 100 com o código: ${smsId}`,
+        body: 'Your call to Emergency Health Line has been registered. ' +
+            'To know your estimated hold time, please send a SMS ' +
+            `to 303 800 100 only with your code: ${smsId}`,
         to: to
     });
 }
 
 
 export async function getPatientHoldTime(smsId: number) {
-    const holdTime = await TextService.getHoldTimeById(smsId);
-
     const twimlResponse = new twiml.MessagingResponse();
-    twimlResponse.message(`O tempo de espera estimado até lhe ligarem é ${holdTime} minutos.`);
+
+    try {
+        const holdTime = await TextService.getHoldTimeById(smsId);
+        twimlResponse.message(`Your estimated holding time is of ${holdTime} minutes.`);
+    } catch (e) {
+        if (e === RequestErrors.NOT_A_VALID_ID) {
+            twimlResponse.message(`Your message is not in the valid format.`);
+        } else {
+            throw e;
+        }
+    }
 
     return twimlResponse;
 }
--- server/src/services/text-service.ts
+++ server/src/services/text-service.ts
@@ -1,12 +1,20 @@
 import {QueueRepository, RequestRepository, StatsRepository} from "../repository";
 import {StatsName} from "../repository/stats-repository";
+import {RequestErrors} from "../utils/request-util";
 
 
 export async function getHoldTimeById(smsId: number) {
+    if (isNaN(smsId)) throw RequestErrors.NOT_A_VALID_ID;
+
+
     const requestId = await RequestRepository.getRequestId(smsId);
     const queuePosition = await QueueRepository.getQueuePosition(requestId);
     const holdtimeDoc = await StatsRepository.getStatsByName(StatsName.ESTIMATED_HOLD_TIME);
     const holdtime = (holdtimeDoc || [])[0];
 
-    return (((holdtime || {}).value || 0) * queuePosition);
+    if(((holdtime || {}).value || 0)) {
+        return 1;
+    } else {
+        return (((holdtime || {}).value || 0) / 60) * queuePosition;
+    }
 }

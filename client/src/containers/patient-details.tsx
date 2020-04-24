b5315752967ace22fb070c512663dae1bd67f83e
Patrícia Pereira <patricia_alexandrapereira@hotmail.com>
2020-04-24 22:46:59 +0100

    #17: Change patient details/call UI.

--- client/src/containers/patient-details.tsx
+++ client/src/containers/patient-details.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
-import { Card, Icon, Grid } from "semantic-ui-react";
+import { Card, Icon, Grid, Segment } from "semantic-ui-react";
 
 import { RootState } from "../store/store";
 import Request from '../model/api/request'
 import BrowserDialler from "./browser-dialler";
 
-// https://react.semantic-ui.com/views/card/#content-extra-contentd
 
 function PatientDetails() {
     const request: Request = useSelector((state: RootState) => state.appReducer.request);
 
     return (
-        <FullHeightGrid divided="vertically">
-            <Grid.Row columns={1}>
+        <FullHeightGrid columns={2} divided>
+            <Grid.Row>
                 <Grid.Column>
-                    <Card.Group centered>
-                        <Card raised>
+                    <Card.Group>
+                        <Card fluid>
                             <Card.Content style={{ backgroundColor: '#e6e6e6' }} textAlign="center">
                                 <Icon name="user" size="huge" />
                             </Card.Content>
@@ -27,7 +26,7 @@ function PatientDetails() {
                                 <Card.Description>{request.patient.age}</Card.Description>
                             </Card.Content>
                         </Card>
-                        <Card raised>
+                        <Card fluid>
                             <Card.Content style={{ backgroundColor: '#e6e6e6' }} textAlign="center">
                                 <Icon name="clipboard outline" size="huge" />
                             </Card.Content>
@@ -38,12 +37,14 @@ function PatientDetails() {
                         </Card>
                     </Card.Group>
                 </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns={2}>
                 <Grid.Column>
-                    <BrowserDialler />
+                    <Segment basic>
+                        <BrowserDialler />
+                    </Segment>
+                    <Segment>
+                        TODO
+                    </Segment>
                 </Grid.Column>
-                <Grid.Column>TODO</Grid.Column>
             </Grid.Row>
         </FullHeightGrid>
     );
@@ -52,12 +53,5 @@ function PatientDetails() {
 const FullHeightGrid = styled(Grid)`
     height: 100%;
 `;
-
-// const Container = styled.div`
-//     width: fit-content;
-//     margin: auto;
-//     height: 100%;
-//     position: relative;
-// `;
 
 export default PatientDetails;
